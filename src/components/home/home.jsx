192c8676536db3a444ad4fed3f6c9201d285a903
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2023-07-27 17:08:50 -0400

    Create home.jsx

---
+++ src/components/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Greeting from "../greeting/greeting";
+
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Greeting />
+    </div>
+  );
+}
+
+export default Home;
