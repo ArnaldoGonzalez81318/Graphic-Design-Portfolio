9fcf674003a0e48c7fb66a019af3ecda9b00dd81
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2023-07-27 17:08:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+
+import Navigation from './components/navigation/navigation';
+import Home from './components/home/home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
       </header>
+      <Home />
     </div>
   );
 }
