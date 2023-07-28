f271fd47f43032e6249afbce2e76422f869dfaa4
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2023-07-28 16:43:20 -0400

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Greeting from "../greeting/greeting";
+import Skills from "../skills/skills";
 
 import './home.css';
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <div className="home">
       <Greeting />
+      <Skills />
     </div>
   );
 }
