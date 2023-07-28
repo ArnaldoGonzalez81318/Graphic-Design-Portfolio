84bd9ea896e4acf2a38636e251a36ff1f4b0ac2c
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2023-07-28 16:43:12 -0400

    Create skills.jsx

---
+++ src/components/skills/skills.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { skills } from "../../Portfolio";
+
+import './skills.css';
+
+const Skills = () => {
+  return (
+    <section className="skills-section" id="skills">
+      <div className="skills-header">
+        <h2>Skills</h2>
+        <p>Here are some of the technologies I've worked with:</p>
+      </div>
+      <div className="skills-container">
+        {skills.data.map((skill, index) => {
+          return (
+            <div className="skill-card" key={index}>
+              <div className="skill-card-content">
+                <div className="skill-name">{skill.name}</div>
+                <p className="skill-description">{skill.description}</p>
+                <button className="skill-btn">Learn More</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
