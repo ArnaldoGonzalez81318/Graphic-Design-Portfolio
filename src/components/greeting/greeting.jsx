8fca61d7bb5252a734cd626d3ee0868ef3df0f94
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2023-07-28 14:17:56 -0400

    Update greeting.jsx

--- src/components/greeting/greeting.jsx
+++ src/components/greeting/greeting.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { greeting } from "../../Portfolio";
+import { greeting, socialMedia } from "../../Portfolio";
 
 import "./greeting.css";
 
 const Greeting = () => {
   return (
     <section className="greeting-section" id="greeting">
+      <div className="greeting-shape-1"></div>
+      <div className="greeting-shape-2"></div>
       <div className="greeting-container">
         <h1 className="greeting-name">
           Hi, I'm <span className="greeting-name-highlight">{greeting.username}</span>
@@ -17,6 +19,38 @@ const Greeting = () => {
         <p className="greeting-description">
           {greeting.description}
         </p>
+        <div className="greeting-social-links">
+          {socialMedia.data.map(socialLink => {
+            return (
+              <a
+                href={socialLink.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+                key={socialLink.name}
+              >
+                {socialLink.icon}
+              </a>
+            );
+          })}
+        </div>
+        <div className="greeting-resume">
+          <a
+            href={greeting.resumeLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button"
+          >
+            Download CV
+          </a>
+        </div>
+        <div className="greeting-scroll-down">
+          <a href="#about">
+            <div className="greeting-scroll-down-button">
+              <div className="greeting-scroll-down-button-line"></div>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
