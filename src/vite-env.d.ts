3f2c2479e3f1d4de0cb188e4ab81e96958906937
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2026-04-08 13:23:01 -0400

    Add VITE_FIREBASE_ARCHIVE_COLLECTION env type
    Expose an optional readonly VITE_FIREBASE_ARCHIVE_COLLECTION on ImportMetaEnv in src/vite-env.d.ts so TypeScript can type-check access to import.meta.env.VITE_FIREBASE_ARCHIVE_COLLECTION (string | undefined).

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_ARCHIVE_COLLECTION?: string;
+}
+
 declare module '*.svg' {
   const src: string;
   export default src;
